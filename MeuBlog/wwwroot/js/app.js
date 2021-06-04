1c2ce59964fecd4e78cd02ab83920063db8fb095
Wagner Villa <wagner.pvilla@gmail.com>
2021-06-04 16:36:00 -0300

    final

--- MeuBlog/wwwroot/js/app.js
+++ MeuBlog/wwwroot/js/app.js
@@ -1,84 +1,130 @@
 const blogService = require("./blogService.js");
 const serviceWorker = require("./swRegister.js");
+const gyroscope = require("./gyroscope.js");
+
 let defferedPrompt;
 blogService.loadLatestBlogPosts();
+gyroscope.init();
 
 if (!"BackgroundFetchManager" in self) {
-  alert("background fetch não está disponível neste site");
-  return;
+    alert("background fetch não está disponível neste site");
+    return;
 }
 
 window.pageEvents = {
-  loadBlogPost(link) {
-    blogService.loadBlogPost(link);
-  },
-  loadMoreBlogPosts() {
-    blogService.loadMoreBlogPosts();
-  },
-
-  tryAddHomeScreen() {
-    defferedPrompt.prompt();
-    defferedPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome == "accepted") {
-        console.log("Usuário aceitou o A2HS prompt");
-        $("#install-container").hide();
-      }
-      defferedPrompt = null;
-    });
-  },
-  setBackgroundFetch(link) {
-    if (!"BackgroundFetchManager" in self) {
-      alert("background fetch não está disponível neste site");
-      return;
+    loadBlogPost(link) {
+        blogService.loadBlogPost(link);
+    },
+    loadMoreBlogPosts() {
+        blogService.loadMoreBlogPosts();
+    },
+    tryAddHomeScreen() {
+        defferedPrompt.prompt();
+        defferedPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome == "accepted") {
+                console.log("Usuário aceitou o A2HS prompt");
+                $("#install-container").hide();
+            }
+            defferedPrompt = null;
+        });
+    },
+    setBackgroundFetch(link) {
+        if (!"BackgroundFetchManager" in self) {
+            alert("background fetch não está disponível neste site");
+            return;
+        }
+        alert("click");
+        navigator.serviceWorker.ready.then(async (swReg) => {
+            const bgFetch = await swReg.backgroundFetch.fetch(
+                link,
+                ["/Home/Post/?link=" + link],
+                {
+                    title: link,
+                    icons: [
+                        {
+                            sizes: "192x192",
+                            src: "images/icons/icon-192x192.png",
+                            type: "image/png",
+                        },
+                    ],
+                    downloadTotal: 15000,
+                }
+            );
+            bgFetch.addEventListener("progress", () => {
+                if (!bgFetch.downloadTotal) return;
+                const percent = Math.round(
+                    (bgFetch.downloaded / bgFetch.downloadTotal) * 100
+                );
+                console.log(`Download progress: ${percent}%`);
+                console.log(`Download status: ${bgFetch.result}`);
+                $(".download-start").hide();
+                $("#status-download").show();
+                $("#status-download > .progress > .progress-bar").css(
+                    "width",
+                    percent + "%"
+                );
+                if (bgFetch.result === "success") {
+                    $("#status-download > .text-success").show();
+                }
+            });
+        });
+    },
+    requestPushPermission: function () {
+        serviceWorker.requestPushPermission();
+    },
+    vibrate() {
+        if ("vibrate" in navigator) {
+            // vibration API supported
+            navigator.vibrate = navigator.vibrate ||
+                navigator.webkitVibrate || navigator.mozVibrate ||
+                navigator.msVibrate;
+            navigator.vibrate([1000]);
+        }
+    },
+    getGeolocation() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                function success(position) {
+                    console.log('latitude ' + position.coords.latitude + ' longitude ' + position.coords.longitude);
+                    window.pageEvents.getAddress(position.coords.latitude, position.coords.longitude);
+                },
+                function error(error_message) {
+                    console.log('An error has occured while retrieving location ' + error_message);
+                });
+        } else {
+            console.log('geolocation is not enabled on this browser');
+        }
+    },
+    getAddress(latitude, longitude) {
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=GOOGLE_KEY`;
+        console.log("url:" + url);
+        $.ajax(url)
+            .then(
+                function success(response) {
+                    console.log({ response });
+                    if (response === undefined || response.results === undefined) {
+                        return;
+                    }
+                    const dataFromCity = response.results.find(obj => obj.types.includes('locality'));
+                    $('#address-found').val(dataFromCity.formatted_address);
+                    $('#address-button').hide();
+                    $('#address-found-input').show();
+                    console.log('User\'s Address Data is ' + response);
+                },
+                function fail(status) {
+                    console.log('Request failed. Returned status of' + status);
+                }
+            );
     }
-    alert("click");
-    navigator.serviceWorker.ready.then(async (swReg) => {
-      const bgFetch = await swReg.backgroundFetch.fetch(
-        link,
-        ["/Home/Post/?link=" + link],
-        {
-          title: link,
-          icons: [
-            {
-              sizes: "192x192",
-              src: "images/icons/icon-192x192.png",
-              type: "image/png",
-            },
-          ],
-          downloadTotal: 15000,
-        }
-      );
-      bgFetch.addEventListener("progress", () => {
-        if (!bgFetch.downloadTotal) return;
-        const percent = Math.round(
-          (bgFetch.downloaded / bgFetch.downloadTotal) * 100
-        );
-        console.log(`Download progress: ${percent}%`);
-        console.log(`Download status: ${bgFetch.result}`);
-        $(".download-start").hide();
-        $("#status-download").show();
-        $("#status-download > .progress > .progress-bar").css(
-          "width",
-          percent + "%"
-        );
-        if (bgFetch.result === "success") {
-          $("#status-download > .text-success").show();
-        }
-      });
-    });
-  },
-  requestPushPermission: function () {
-    serviceWorker.requestPushPermission();
-  },
 };
 
 window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  defferedPrompt = e;
+    e.preventDefault();
+    defferedPrompt = e;
 
-  $("#install-container").show();
+    $("#install-container").show();
 });
 
 window.addEventListener("appinstalled", (evt) => {
-  console.log("app foi adicionada na home screen!  Yuhuu!");
+    console.log("app foi adicionada na home screen!  Yuhuu!");
 });
---
+++ MeuBlog/wwwroot/js/gyroscope.js
@@ -0,0 +1,47 @@
+﻿define([], function () {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    function init() {
+        if ('Gyroscope' in window) {
+            let gyroscope = new Gyroscope();
+            gyroscope.addEventListener('reading', e =>
+                rotationHandler({
+                    alpha: gyroscope.x,
+                    beta: gyroscope.y,
+                    gamma: gyroscope.z
+                }));
+            gyroscope.start();
+        }
+    }
+
+    function rotationHandler(rotation) {
+        var info, xyz = "[X, Y, Z]";
+        var alpha; var beta; var gamma;
+        if (rotation.alpha && rotation.beta &&
+            rotation.gamma) {
+            alpha = rotation.alpha.toFixed(2) * 100;
+            beta = rotation.beta.toFixed(2) * 100;
+            gamma = rotation.gamma.toFixed(2) * 100;
+        }
+        info = xyz.replace("X", alpha);
+        info = info.replace("Y", beta);
+        info = info.replace("Z", gamma);
+        $('#rotation-info').html(info);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(100, 100, alpha, 10);
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(120, 120, beta, 10);
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(140, 140, gamma, 10);
+        var interval = setInterval(function () {
+            ctx.clearRect(0, 0, canvas.width,
+                canvas.height);
+        }, 50000);
+    }
+
+    return {
+        init
+    }
+
+});
